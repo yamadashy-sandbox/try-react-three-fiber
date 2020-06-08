69cdb18caff534f50d856f167511de9279b22be7
Kazuki Yamada <koukun0120@gmail.com>
2020-06-08 22:42:15 +0900

    add: demo

---
+++ build/precache-manifest.c22463cb791478389f519e3a479af448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63825c1d538f99a7be448251e3c4f4b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c54dd0d5a63e0c8251f1",
+    "url": "/static/css/main.41d6cdfa.chunk.css"
+  },
+  {
+    "revision": "50cd015347effa72eec2",
+    "url": "/static/js/2.d80dbaba.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/static/js/2.d80dbaba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54dd0d5a63e0c8251f1",
+    "url": "/static/js/main.950b6eee.chunk.js"
+  },
+  {
+    "revision": "a4b2dcff44c8d90143da",
+    "url": "/static/js/runtime-main.071e7a7d.js"
+  }
+]);
