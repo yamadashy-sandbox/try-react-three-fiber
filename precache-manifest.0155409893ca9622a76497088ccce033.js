35c3b9984ec30b0eec57226eb3b949a507e38e56
Kazuki Yamada <koukun0120@gmail.com>
2020-06-09 01:02:39 +0900

    Updates

---
+++ precache-manifest.0155409893ca9622a76497088ccce033.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "398fdad0c890bee6386a",
+    "url": "/try-react-three-fiber/static/js/runtime~main.fbde5ea9.js"
+  },
+  {
+    "revision": "513b495c3e2e91b04941",
+    "url": "/try-react-three-fiber/static/js/main.fbefe9be.chunk.js"
+  },
+  {
+    "revision": "952e8f785e909c893adb",
+    "url": "/try-react-three-fiber/static/js/2.25662ca9.chunk.js"
+  },
+  {
+    "revision": "513b495c3e2e91b04941",
+    "url": "/try-react-three-fiber/static/css/main.946891f5.chunk.css"
+  },
+  {
+    "revision": "8c5c972516f896fc41bfe9d0b900eb84",
+    "url": "/try-react-three-fiber/index.html"
+  }
+];
